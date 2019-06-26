23122c04f39a015821e1b1e4d5c2548a232dffbf
Just1689 <Just1689@gmail.com>
2019-06-26 19:07:46 +0200

    stubbed message handler

--- src/main/java/com/team142/tode/controller/messages/HandlePPlace.java
+++ src/main/java/com/team142/tode/controller/messages/HandlePPlace.java
@@ -1,11 +1,17 @@
 package com.team142.tode.controller.messages;
 
+
 import lombok.NoArgsConstructor;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @NoArgsConstructor
 public class HandlePPlace implements Handler {
+    private Logger LOG = Logger.getLogger(HandlePPlace.class.getName());
+
     @Override
     public void handle(String sessionID, String msg) {
-
+        LOG.log(Level.SEVERE, "handle Message P Place not implemented");
     }
 }
