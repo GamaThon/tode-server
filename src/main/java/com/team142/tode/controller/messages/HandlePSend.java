8884b58e6ac3969622f28eb0eb21099bb491f018
Just1689 <Just1689@gmail.com>
2019-06-26 19:08:27 +0200

    stubbed message handler

--- src/main/java/com/team142/tode/controller/messages/HandlePSend.java
+++ src/main/java/com/team142/tode/controller/messages/HandlePSend.java
@@ -2,10 +2,15 @@ package com.team142.tode.controller.messages;
 
 import lombok.NoArgsConstructor;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @NoArgsConstructor
 public class HandlePSend implements Handler {
+    private Logger LOG = Logger.getLogger(HandlePSend.class.getName());
+
     @Override
     public void handle(String sessionID, String msg) {
-
+        LOG.log(Level.SEVERE, "handle Message P Send not implemented");
     }
 }
