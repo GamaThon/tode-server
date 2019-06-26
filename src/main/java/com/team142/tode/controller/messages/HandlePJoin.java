22319ff2e697817e5bf57e72b4b6c604ab454369
Just1689 <Just1689@gmail.com>
2019-06-26 18:40:24 +0200

    handle join game

--- src/main/java/com/team142/tode/controller/messages/HandlePJoin.java
+++ src/main/java/com/team142/tode/controller/messages/HandlePJoin.java
@@ -1,11 +1,24 @@
 package com.team142.tode.controller.messages;
 
+import com.team142.tode.model.TDServer;
+import com.team142.tode.model.messages.MessagePJoin;
+import com.team142.tode.utils.JsonUtils;
 import lombok.NoArgsConstructor;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @NoArgsConstructor
 public class HandlePJoin implements Handler {
+
+    private static final Logger LOG = Logger.getLogger(HandlePJoin.class.getName());
+
     @Override
     public void handle(String sessionID, String msg) {
-
+        MessagePJoin o = (MessagePJoin) JsonUtils.jsonToObject(msg, MessagePJoin.class);
+        boolean ok = TDServer.instance.getGames().get(o.getId()).playerJoins(TDServer.instance.getPlayers().get(sessionID));
+        if (!ok) {
+            LOG.log(Level.INFO, "Could not join the game: ", o.getId());
+        }
     }
 }
---
+++ src/main/java/com/team142/tode/model/messages/MessagePJoin.java
@@ -0,0 +1,10 @@
+package com.team142.tode.model.messages;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class MessagePJoin extends BaseMessage {
+    private String id;
+}
