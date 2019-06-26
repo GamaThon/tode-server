be253e700ed20d9b2af267cf263e6f9463d9d107
Just1689 <Just1689@gmail.com>
2019-06-26 18:20:50 +0200

    new message type

---
+++ src/main/java/com/team142/tode/controller/messages/HandlePNew.java
@@ -0,0 +1,11 @@
+package com.team142.tode.controller.messages;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class HandlePNew implements Handler {
+    @Override
+    public void handle(String sessionID, String msg) {
+
+    }
+}
