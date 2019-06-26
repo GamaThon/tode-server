827dfb0d1576cba522d1de1321d9212b81cbca9d
Just1689 <Just1689@gmail.com>
2019-06-26 18:44:44 +0200

    handle name message

--- src/main/java/com/team142/tode/controller/messages/HandlePName.java
+++ src/main/java/com/team142/tode/controller/messages/HandlePName.java
@@ -1,11 +1,15 @@
 package com.team142.tode.controller.messages;
 
+import com.team142.tode.model.TDServer;
+import com.team142.tode.model.messages.MessagePName;
+import com.team142.tode.utils.JsonUtils;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class HandlePName implements Handler {
     @Override
     public void handle(String sessionID, String msg) {
-        //
+        MessagePName o = (MessagePName) JsonUtils.jsonToObject(msg, MessagePName.class);
+        TDServer.instance.getPlayers().get(sessionID).setName(o.getName());
     }
 }
---
+++ src/main/java/com/team142/tode/model/messages/MessagePName.java
@@ -0,0 +1,10 @@
+package com.team142.tode.model.messages;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class MessagePName extends BaseMessage {
+    @Getter
+    private String name;
+}
