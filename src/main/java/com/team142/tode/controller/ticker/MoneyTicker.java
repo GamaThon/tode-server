6d07de0fe35358ba012da5dc6f08881f551810b1
Justin Tamblyn <just1689@gmail.com>
2019-06-27 13:54:43 +0200

    handler

--- src/main/java/com/team142/tode/controller/ticker/MoneyTicker.java
+++ src/main/java/com/team142/tode/controller/ticker/MoneyTicker.java
@@ -1,36 +1,25 @@
 package com.team142.tode.controller.ticker;
 
+
 import com.team142.tode.model.Game;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+public class MoneyTicker extends Ticker {
 
-public class MoneyTicker implements Runnable, Stoppable {
+    private Game game;
 
-    private final Game game;
-    private final long interval;
-    private AtomicBoolean keepOnKeepingOn;
-
-    public MoneyTicker(Game game, long interval) {
+    public MoneyTicker(long interval, Game game) {
+        super(interval);
         this.game = game;
-        this.interval = interval;
-        this.keepOnKeepingOn = new AtomicBoolean(true);
     }
 
     @Override
-    public void run() {
-        while (keepOnKeepingOn.get()) {
-            int m = game.getMoneyRate();
-            game.getPlayers().values().forEach((p) -> p.getMoney().addAndGet(m));
-            try {
-                Thread.sleep(interval);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+    public void handle() {
+        int m = game.getMoneyRate();
+        game.getPlayers().
+                values().
+                forEach(
+                        (p) -> p.getMoney().addAndGet(m)
+                );
     }
 
-    @Override
-    public void stop() {
-        this.keepOnKeepingOn.set(false);
-    }
 }
