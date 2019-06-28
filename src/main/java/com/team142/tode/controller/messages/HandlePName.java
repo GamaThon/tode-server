3a0a114adf6b44158f53fcaea96e074104da1271
Justin Tamblyn <just1689@gmail.com>
2019-06-28 18:42:00 +0200

    handle incoming name better

--- src/main/java/com/team142/tode/controller/messages/HandlePName.java
+++ src/main/java/com/team142/tode/controller/messages/HandlePName.java
@@ -1,15 +1,26 @@
 package com.team142.tode.controller.messages;
 
+import com.team142.tode.model.Player;
 import com.team142.tode.model.Server;
+import com.team142.tode.model.ViewType;
 import com.team142.tode.model.messages.MessagePName;
 import com.team142.tode.utils.JsonUtils;
 import lombok.NoArgsConstructor;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @NoArgsConstructor
 public class HandlePName implements Handler {
+    private static final Logger LOG = Logger.getLogger(HandlePName.class.getName());
+
     @Override
     public void handle(String sessionID, String msg) {
         MessagePName o = (MessagePName) JsonUtils.jsonToObject(msg, MessagePName.class);
-        Server.instance.getPlayers().get(sessionID).setName(o.getName());
+        LOG.log(Level.INFO, "Name is " + o.getName());
+        Player player = Server.instance.getPlayers().get(sessionID);
+        player.setName(o.getName());
+        player.changePlayerView(ViewType.MATCHING);
+
     }
 }
